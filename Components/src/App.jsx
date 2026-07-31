5419983ada0469675e28c3774bed67e346443177
arvajKhan <arvajkhan194@gmail.com>
2026-07-31 22:50:53 +0530

    gh

--- Components/src/App.jsx
+++ Components/src/App.jsx
@@ -1,13 +1,15 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 // import './App.css'
-
+import Hello from "./assets/hello"
 function App() {
   return (
     <>
     <h1>App Components</h1>
+    <Hello/>
+    <img src={reactLogo}/>
     </>
   )
 }
--- Components/src/assets/hello.jsx
+++ Components/src/assets/hello.jsx
@@ -1,2 +1,6 @@
-q
-q
+
+function Hello(){
+  return <h2>Hello Component</h2>
+}
+
+export default Hello
