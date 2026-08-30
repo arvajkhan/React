1685010b336f5724f66524e8a851585901d8651c
arvajKhan <arvajkhan194@gmail.com>
2026-08-30 17:24:34 +0530

    kl

--- 04 ArraysAndObject/src/App.jsx
+++ 04 ArraysAndObject/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Arrays from "./Arrays";
+import Object from "./Object";
 function App() {
   return (
     <>
       <Arrays />
+      <Object />
     </>
   );
 }
---
+++ 04 ArraysAndObject/src/Object.jsx
@@ -0,0 +1,13 @@
+function Object(){
+  const obj = {firstName: "Arvaj",lastName: "khan", age: 25, college: "xyz"}
+  return(
+    <>
+    <h2>Person Details</h2>
+    <p>first Name : {user.firstName} </p>
+    <p>Last Name : {user.lastName} </p>
+    <p>Age : {user.age} </p>
+    <p>College : {user.college} </p>
+    </>
+  )
+}
+export default Object
